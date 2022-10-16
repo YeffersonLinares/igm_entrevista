c0da57610df807f1281c56028d7e742eb1f15413
Jefferson Linares <ylinares@unbosque.edu.co>
2022-10-16 00:02:06 -0500

    askdjas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        \App\Models\User::factory()->create([
+        \App\Models\User::create([
             'name' => 'Test User',
             'email' => 'test@example.com',
             'password' => Hash::make('1234'),
